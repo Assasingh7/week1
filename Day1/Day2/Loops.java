ae07a370df081f45f8b7416023f8a6ef8f947f3d
Assasingh7 <109212099+Assasingh7@users.noreply.github.com>
2024-07-14 12:30:03 +0530

    Second Commit

---
+++ Day1/Day2/Loops.java
@@ -0,0 +1,64 @@
+package Day1.Day2;
+
+import java.util.Scanner;
+
+/**
+ * Loops
+ */
+public class Loops {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        // for (int i = 1; i <=n; i++) {
+        //     for (int j = 1; j <=n; j++) {
+        //         System.out.print("*"+" ");
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i = 1; i <=n; i++) {
+        //     for(int k=1;k<=(n-i);k++){
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <=i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i = n; i >=1; i--) {
+        //     for (int j = 1; j <=i; j++) {
+        //         System.out.print("*"+" ");
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i = n; i >=1; i--) {
+        //     for(int k=1;k<=(n-i);k++){
+        //         System.out.print("  ");
+        //     }
+        //     for (int j = 1; j <=i; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i = n; i >=1; i--) {
+        //     for(int k=1;k<=(n-i);k++){
+        //         System.out.print("    ");
+        //     }
+        //     for (int j = 1; j <=i; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i = 1; i <=n; i++) {
+        //     for (int j = 1; j <=n; j++) {
+        //         if(i==1 || j==1 || j==n || i==n){
+        //             System.out.print("* ");
+        //         }else{
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        sc.close();
+    }
+}
