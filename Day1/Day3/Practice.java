b211eadd98c4b7562b15ba40704175138d68d211
Assasingh7 <109212099+Assasingh7@users.noreply.github.com>
2024-07-15 20:47:47 +0530

    Update Practice.java

--- Day1/Day3/Practice.java
+++ Day1/Day3/Practice.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Practice{
 public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
-    int n=6;
+    int n=sc.nextInt();
     // for (int i = 1; i <=n; i++) {
     //     for (int j = 1; j <=n; j++) {
     //         if(i==j || i+j==n+1){
