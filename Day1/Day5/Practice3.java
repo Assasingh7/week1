141a79d883eafb4fa5bde26cad41f64b3ffa7e37
Assasingh7 <109212099+Assasingh7@users.noreply.github.com>
2024-07-18 16:18:18 +0530

    first commit

--- Day1/Day5/Practice3.java
+++ Day1/Day5/Practice3.java
@@ -1,5 +1,59 @@
 package Day1.Day5;
 
 public class Practice3 {
-    
+    public static void main(String[] args) {
+//         int n=4;
+//         for(int i=1;i<=n;i++){
+//             for(int k=1;k<=n-i;k++){
+//                 System.out.print("  ");
+//             }
+//             for (int j = 1; j <=i; j++) {
+//  if(j==1)System.out.print("* ");
+//  else System.out.print("  ");               
+//             }
+//             for(int k=3;k<=i && k<=n;k++){
+//                 System.out.print("  ");
+//             }
+//             for(int j=1;j<=1;j++){
+//                 if(i>1)
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//         for(int i=n-1;i>=1;i--){
+//             for(int k=1;k<=n-i;k++){
+//                 System.out.print("  ");
+//             }
+//             for (int j = 1; j <=i; j++) {
+//  if(j==1)System.out.print("* ");
+//  else System.out.print("  ");               
+//             }
+//             for(int k=3;k<=i && k<=n;k++){
+//                 System.out.print("  ");
+//             }
+//             for(int j=1;j<=1;j++){
+//                 if(i>1)
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+int n=4;
+for(int i=1;i<=n;i++){
+    for(int k=1;k<=n-i;k++){
+        System.out.print("  ");
+    }
+    for (int j = 1; j <=i; j++) {
+if(j==1)System.out.print(i+" ");
+else System.out.print("0 ");               
+    }
+    for(int k=3;k<=i && k<=n;k++){
+        System.out.print("0 ");
+    }
+    for(int j=1;j<=1;j++){
+        if(i>1)
+        System.out.print(i+" ");
+    }
+    System.out.println();
+}
+    }
 }
