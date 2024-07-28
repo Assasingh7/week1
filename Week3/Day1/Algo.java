af8d8d88ff87b9830540f58fe5824bacc4bc5765
Assasingh7 <109212099+Assasingh7@users.noreply.github.com>
2024-07-28 17:26:11 +0530

    first commit

--- Week3/Day1/Algo.java
+++ Week3/Day1/Algo.java
@@ -9,6 +9,19 @@ static int sum(int[] nums,int a,int b){
     }
     return sum;
 }
+static int subArray(int[] arr){
+    int max = Integer.MIN_VALUE;
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+        
+    for (int j = i; j < arr.length; j++) {
+        sum = sum + arr[i];
+        max=Math.max(max, sum);
+    }
+        
+    }
+    return max;
+}
 
     public static void main(String[] args) {
         
