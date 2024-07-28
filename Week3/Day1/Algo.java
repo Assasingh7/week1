d7f125c3ea5cb44b4c392555dbac2c10cf025e51
Assasingh7 <109212099+Assasingh7@users.noreply.github.com>
2024-07-28 12:14:38 +0530

    first

---
+++ Week3/Day1/Algo.java
@@ -0,0 +1,17 @@
+package Week3.Day1;
+
+public class Algo {
+static int sum(int[] nums,int a,int b){
+    int sum=0;
+    for(int i=a;i<=b;i++){
+        sum+=nums[i];
+
+    }
+    return sum;
+}
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
